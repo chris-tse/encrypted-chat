e43dee0799b5dad84ba23645024b8ed00e3f76fd
Charlie Liu <charlie.liu-1@ou.edu>
2018-11-30 22:35:39 -0600

    checking to see if the modifications are saved

--- public/js/chat.js
+++ public/js/chat.js
@@ -27,6 +27,15 @@ $(function () {
 
     let nickname = getCookie('nickname');
 
+    function encryptDES(message, key) {
+      var keyHex = CryptoJS.enc.Utf8.parse(key);
+      var encrypted = CryptoJS.DES.encrypt(message, keyHex, {
+        mode: CryptoJS.mode.ECB,
+        padding: CryptoJS.pad.Pkcs7
+      });
+      return encrypted.toString();
+    }
+
     function sendMessage() {
         let msg = $("#inputMessage").val();
         var encrypted = encryptDES(msg, getCookie('chatkey'));
@@ -36,15 +45,6 @@ $(function () {
             $("#inputMessage").val("");
         }
         return false;
-    }
-
-    function encryptDES(message, key) {
-      var keyHex = CryptoJS.enc.Utf8.parse(key);
-      var encrypted = CryptoJS.DES.encrypt(message, keyHex, {
-        mode: CryptoJS.mode.ECB,
-        padding: CryptoJS.pad.Pkcs7
-      });
-      return encrypted.toString();
     }
 
     $(window).keydown(e => {
