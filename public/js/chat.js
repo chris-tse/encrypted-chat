ad2c459e159b133d021073aae00ac36b6d68622d
Charlie Liu <charlie.liu-1@ou.edu>
2018-11-30 22:26:46 -0600

    added encryptDES function, and added it to sendMessage to print encryptedMessage

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,7 +5,7 @@ $(function () {
         var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
         return v ? v[2] : null;
     }
-    
+
     const updateTyping = () => {
         if (connected) {
             if (!typing) {
@@ -24,42 +24,52 @@ $(function () {
             }, TYPING_TIMER_LENGTH);
         }
     }
-    
+
     let nickname = getCookie('nickname');
-    
+
     function sendMessage() {
         let msg = $("#inputMessage").val();
+        var encrypted = encryptDES(msg, getCookie('chatkey'));
         if (msg === undefined || msg.length > 0) {
             socket.emit("chat message", {sender: nickname, message: msg});
+            socket.emit("chat message", {sender: nickname, message: encrypted});
             $("#inputMessage").val("");
         }
         return false;
     }
-    
-    
+
+    function encryptDES(message, key) {
+      var keyHex = CryptoJS.enc.Utf8.parse(key);
+      var encrypted = CryptoJS.DES.encrypt(message, keyHex, {
+        mode: CryptoJS.mode.ECB,
+        padding: CryptoJS.pad.Pkcs7
+      });
+      return encrypted.toString();
+    }
+
     $(window).keydown(e => {
         if (!(e.ctrlKey || e.metaKey || e.altKey)) {
             $('#inputMessage').focus();
         }
-        
+
         if (e.which === 13) {
             sendMessage();
             socket.emit('stop typing');
             typing = false;
         }
     });
-    
+
     $('#inputMessage').on('input', () => {
         updateTyping();
     })
-    
+
     let socket = io();
-    
+
     $("form").submit(function (e) {
         e.preventDefault();
-        
+
     });
-    
+
     socket.on('chat message', function(msg){
         $('#messages').append($('<li>').text(`${msg.sender}: ${msg.message}`));
     });
