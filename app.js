2da89c73f2e29c5d17cd17817be8547ab815d22a
Christopher Tse <chris@chris-tse.com>
2018-12-04 23:24:19 -0600

    Remove unused packages

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const fs = require('fs');
 const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 const crypto = require('crypto');
 const cryptoUtils = require('./cryptoutils');
 const exphbs = require('express-handlebars');
@@ -23,9 +21,7 @@ app.set('view engine', 'hbs');
 app.engine('hbs', exphbs({
     extname: 'hbs'
 }));
-app.use(bodyParser.json({limit: '5mb'}));
-app.use(bodyParser.urlencoded({limit: '5mb', extended: false, parameterLimit:300}));
-app.use(cookieParser());
+
 app.use(express.static('node_modules/crypto-js'));
 app.use(express.static('public'));
 
@@ -34,29 +30,7 @@ app.get('/', (req, res) => {
     res.render('login');
 });
 
-// app.post('/login', (req, res) => {
-//     let {nickname, hashedPW: receivedHash} = req.body;
-
-//     if (receivedHash === computedHash) {
-//         console.log('right pw');
-//         let resObj = {chatkey, PW: process.env.PW};
-//         let payload = cryptoUtils.encryptDES(JSON.stringify(resObj), process.env.PW).toString();
-//         // console.log('Sending encrypted:');
-//         // console.log(resObj);
-//         // console.log(payload);
-//         return res.status(200).json({message: 'Correct password', payload});
-//         // res.cookie('chatkey', encryptKey, {maxAge: 600000, httpOnly:false, overwrite: true});
-//         // res.cookie('nickname', nickname, {maxAge: 600000, httpOnly:false, overwrite: true});
-//         // res.redirect('/chat');
-//     } else {
-//         return res.status(401).json({message: 'Incorrect password'});
-//     }
-// });
-
 app.get('/chat', (req, res) => {
-    if (!req.cookies.password) {
-        return res.redirect('/');
-    }
     res.render('chat');
 });
 
@@ -78,18 +52,7 @@ io.on('connection', socket => {
 
 
 server.listen(port, () => {
-    // if (process.env.PW == null || process.env.PW == '') {
-    //     console.error('Set password with the PW environment variable');
-    //     console.error('Example:');
-    //     console.error('$ PW=<password> npm start');
-    //     return process.exit(1);
-    // }
-    
-    // let hash = crypto.createHash('SHA512');
-    // hash.update(process.env.PW);
-    // computedHash = hash.digest('hex');
-    // chatkey = crypto.randomBytes(7).toString('hex');
-    
+    // Development use only
     console.log(`Server running at http://127.0.0.1:${port}`);
     
     // Ping heroku server to keep from sleeping
