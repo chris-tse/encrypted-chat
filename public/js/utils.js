7804a6469ce4d2390ef774b7d530129762b860ff
Christopher Tse <chris@chris-tse.com>
2018-12-04 23:29:19 -0600

    Remove unused functions

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,17 +1,3 @@
-// Cookie utility functions from https://plainjs.com/javascript/utilities/set-cookie-get-cookie-and-delete-cookie-5/
-function getCookie(name) {
-    var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
-    return v ? v[2] : null;
-}
-
-function setCookie(name, value, days) {
-    var d = new Date;
-    d.setTime(d.getTime() + 24*60*60*1000*days);
-    document.cookie = name + "=" + value + ";path=/;expires=" + d.toGMTString();
-}
-
-function deleteCookie(name) { setCookie(name, '', -1); }
-
 /**
  * Utility function for calling DES encrypt from Crypto-JS library
  * @param {string} message Message to be encrypted
