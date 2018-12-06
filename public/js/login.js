cd7e985e039c329af7b51a27bea60dee301bf1bb
Christopher Tse <chris@chris-tse.com>
2018-12-06 15:39:19 -0600

    Remove unused code

--- public/js/login.js
+++ public/js/login.js
@@ -8,32 +8,4 @@ $('#login').submit(e => {
     localStorage.setItem('nickname', nickname);
     localStorage.setItem('password', pw);
     window.location.href = '/chat';
-    
-    // let hashedPW = CryptoJS.SHA512(pw).toString();
-    
-    // axios.post('/login', {
-    //     nickname,
-    //     hashedPW
-    // })
-    // .then(function (response) {
-    //     let {payload} = response.data;
-    //     let decryptedPayload = JSON.parse(decryptDES(payload, pw));
-
-    //     if (decryptedPayload.PW === pw) {
-    //         // console.log('same pw');
-    //         deleteCookie('nickname');
-    //         setCookie('nickname', nickname, 600000); 
-    //         setCookie('chatkey', decryptedPayload.chatkey, 600000);
-    //     } else {
-    //         alert('Password doesn\'t match, potentially unsafe server');
-    //     }
-    // })
-    // .catch(function (error, res) {
-    //     console.log(error, res);
-    //     $('#password').addClass('is-invalid');
-    // });
-    
-    // $('#password').change(e => {
-    //     $('#password').removeClass('is-invalid');
-    // })
 })
